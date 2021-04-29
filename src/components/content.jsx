65d37e608dfb0aba42d92f028cf3eed73c1dc490
Dave Doro <sirdoro1@gmail.com>
2021-04-29 20:33:46 +0100

    update on tetsing

---
+++ src/components/content.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                <span>Olodo FC</span>
+            </div>
+        );
+    }
+}
+
+export default Content;
